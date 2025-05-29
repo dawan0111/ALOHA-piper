3a60f4ef94d198df7c61aaa3ea99a65cd1efa8bb
KDW <msd030428@gmail.com>
2025-05-29 13:50:00 +0900

    Create act_bringup package

---
+++ act_bringup/launch/robot.launch.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+#
+# Copyright 2025 AIRO LABS., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import os
+from pathlib import Path  # noqa: E402
+import sys
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path)
+
+from launch.launch_description import LaunchDescription
+from camera_config import CameraConfig, USB_CAM_DIR  # noqa: E402
+
+from launch import LaunchDescription  # noqa: E402
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+CAMERAS = []
+CAMERAS.append(
+    CameraConfig(
+        name='camera1',
+        param_path=Path(USB_CAM_DIR, 'config', 'params_1.yaml')
+    ),
+)
+CAMERAS.append(
+    CameraConfig(
+        name='camera2',
+        param_path=Path(USB_CAM_DIR, 'config', 'params_2.yaml')
+    ),
+)
+CAMERAS.append(
+    CameraConfig(
+        name='camera3',
+        param_path=Path(USB_CAM_DIR, 'config', 'params_3.yaml')
+    ),
+)
+CAMERAS.append(
+    CameraConfig(
+        name='camera4',
+        param_path=Path(USB_CAM_DIR, 'config', 'params_4.yaml')
+    ),
+)
+
+def generate_launch_description() -> LaunchDescription:
+    container = ComposableNodeContainer(
+            name='my_container',
+            namespace='',
+            package='rclcpp_components',
+            executable='component_container',
+            composable_node_descriptions=[
+                ComposableNode(
+                    package='usb_cam',
+                    plugin='usb_cam::UsbCamNode',
+                    name=camera.name,
+                    namespace=camera.namespace,
+                    remappings=camera.remappings,
+                    parameters=[camera.param_path]
+                ) for camera in CAMERAS
+            ],
+            output='screen',
+    )
+
+    return LaunchDescription([
+        container
+    ])
